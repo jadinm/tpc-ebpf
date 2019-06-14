d8b650ab078d89d8cb6799abbd7147a4ac92fd59
Fabien Duchene <fab@ajb.lu>
2019-06-14 11:18:59 +0000

    includes

---
+++ kernel.h
@@ -0,0 +1,95 @@
+/* IP, IPv6 addr, TCP and UDP header structures extracted from the kernel */
+struct iphdr {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8	ihl:4,
+		version:4;
+#elif defined (__BIG_ENDIAN_BITFIELD)
+	__u8	version:4,
+  		ihl:4;
+#else
+#error	"Please fix <asm/byteorder.h>"
+#endif
+	__u8	tos;
+	__be16	tot_len;
+	__be16	id;
+	__be16	frag_off;
+	__u8	ttl;
+	__u8	protocol;
+	__sum16	check;
+	__be32	saddr;
+	__be32	daddr;
+	/*The options start here. */
+};
+
+struct udphdr {
+	__be16	source;
+	__be16	dest;
+	__be16	len;
+	__sum16	check;
+};
+
+struct in6_addr {
+	union {
+		__u8		u6_addr8[16];
+		__be16		u6_addr16[8];
+		__be32		u6_addr32[4];
+	} in6_u;
+#define s6_addr			in6_u.u6_addr8
+#define s6_addr16		in6_u.u6_addr16
+#define s6_addr32		in6_u.u6_addr32
+};
+
+struct ipv6hdr {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8			priority:4,
+				version:4;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u8			version:4,
+				priority:4;
+#else
+#error	"Please fix <asm/byteorder.h>"
+#endif
+	__u8			flow_lbl[3];
+
+	__be16			payload_len;
+	__u8			nexthdr;
+	__u8			hop_limit;
+
+	struct	in6_addr	saddr;
+	struct	in6_addr	daddr;
+};
+
+struct tcphdr {
+	__be16	source;
+	__be16	dest;
+	__be32	seq;
+	__be32	ack_seq;
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u16	res1:4,
+		doff:4,
+		fin:1,
+		syn:1,
+		rst:1,
+		psh:1,
+		ack:1,
+		urg:1,
+		ece:1,
+		cwr:1;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u16	doff:4,
+		res1:4,
+		cwr:1,
+		ece:1,
+		urg:1,
+		ack:1,
+		psh:1,
+		rst:1,
+		syn:1,
+		fin:1;
+#else
+#error	"Adjust your <asm/byteorder.h> defines"
+#endif	
+	__be16	window;
+	__sum16	check;
+	__be16	urg_ptr;
+};
---
+++ utils.h
@@ -0,0 +1,112 @@
+/* Defining constant values */
+
+#define IPPROTO_TCP 6 /* TCP protocol in HDR */
+#define AF_INET6 10 /* IPv6 HDR */
+#define SOL_IPV6 41 /* IPv6 Sockopt */
+#define IPV6_RTHDR 57 /* SRv6 Option for sockopt */
+#define ETH_HLEN 14 /* Ethernet hdr length */
+#define DEBUG 1
+#define PIN_NONE		0
+#define PIN_GLOBAL_NS		2
+
+
+/* eBPF definitions */
+#ifndef __section
+# define __section(NAME)                  \
+   __attribute__((section(NAME), used))
+#endif
+
+#ifndef __inline
+# define __inline                         \
+   inline __attribute__((always_inline))
+#endif
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+
+/* Only use this for debug output. Notice output from bpf_trace_printk()
+ *  * end-up in /sys/kernel/debug/tracing/trace_pipe
+ *   */
+#define bpf_debug(fmt, ...)						\
+			({						\
+			char ____fmt[] = fmt;				\
+			bpf_trace_printk(____fmt, sizeof(____fmt),	\
+			##__VA_ARGS__);					\
+			})
+
+#define htonll(x) ((bpf_htonl(1)) == 1 ? (x) : ((uint64_t)bpf_htonl((x) & \
+				0xFFFFFFFF) << 32) | bpf_htonl((x) >> 32))
+#define ntohll(x) ((bpf_ntohl(1)) == 1 ? (x) : ((uint64_t)bpf_ntohl((x) & \
+				0xFFFFFFFF) << 32) | bpf_ntohl((x) >> 32))
+
+/* IPv6 address */
+struct ip6_addr_t {
+	unsigned long long hi;
+	unsigned long long lo;
+} __attribute__((packed));
+
+/* SRH definition */
+struct ip6_srh_t {
+	unsigned char nexthdr;
+	unsigned char hdrlen;
+	unsigned char type;
+	unsigned char segments_left;
+	unsigned char first_segment;
+	unsigned char flags;
+	unsigned short tag;
+
+	struct ip6_addr_t segments[0];
+} __attribute__((packed));
+/*
+struct fab_flow_tuple {
+	__u32 family;
+	__be32 local_addr[4];
+	__be32 remote_addr[4];
+	__be16 local_port;
+	__be16 remote_port;	
+};*/
+
+struct fab_test_key {
+	__u32 family;
+	__be32 local_addr[4];
+	__be32 remote_addr[4];
+	__be16 local_port;
+	__be16 remote_port;	
+	/*__u32 a;
+	__be32 b;
+	__be16 c;
+	__be16 d;
+	__be32 e[4];
+	__be32 f[4];*/
+};
+struct flow_infos {
+	__u32 srh_id;
+	__u32 last_retransmit;
+	__u32 curr_threshold;
+};
+
+struct bpf_elf_map {
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
+};
+
+struct bpf_elf_map SEC("maps") srh_map = {
+	.type		= BPF_MAP_TYPE_ARRAY,
+	.size_key	= sizeof(uint32_t),
+	.size_value	= 72,
+	.pinning	= PIN_NONE,
+	.max_elem	= 3,
+};
+
+struct bpf_elf_map SEC("maps") conn_map = {
+	.type		= BPF_MAP_TYPE_HASH,
+	/*.size_key	= sizeof(struct bpf_sock_tuple),*/
+	.size_key	= sizeof(struct fab_test_key),
+	.size_value	= sizeof(struct flow_infos),
+	.pinning	= PIN_NONE,
+	.max_elem	= 3,
+};
