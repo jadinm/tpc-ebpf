73fc9ccc18a880ff845b26692a870ccec80a70a6
Mathieu Jadin <mathjadin@gmail.com>
2019-12-20 12:07:55 +0100

    Add first implem of EXP3

--- ebpf_socks_ecn.c
+++ ebpf_socks_ecn.c
@@ -17,20 +17,27 @@
 #define ntohll(x) ((bpf_ntohl(1)) == 1 ? (x) : ((uint64_t)bpf_ntohl((x) & \
 				0xFFFFFFFF) << 32) | bpf_ntohl((x) >> 32))
 
-static int move_path(struct dst_infos *dst_infos, __u32 key, struct bpf_sock_ops *skops)
+static __always_inline int move_path(struct dst_infos *dst_infos, __u32 key, struct bpf_sock_ops *skops)
 {
 	int rv = 1;
 	struct ip6_srh_t *srh = NULL;
+	__u32 bw = 0;
 	// Check needed to avoid verifier complaining about unbounded access
 	// The check needs to be placed very near the actual line
 	if (key >= 0 && key < MAX_SRH_BY_DEST) {
 		srh = &(dst_infos->srhs[key].srh);
+		bw = dst_infos->srhs[key].curr_bw * (1000 / 8) * 1000 * 9 / 10; // max 0.9 the bw
 		rv = bpf_setsockopt(skops, SOL_IPV6, IPV6_RTHDR, srh, sizeof(*srh));
+		if (!rv) {
+			rv = bpf_setsockopt(skops, SOL_SOCKET,
+					    SO_MAX_PACING_RATE, &bw, sizeof(int));
+			//bpf_debug("Try to set max pacing rate to %u returned %d\n", bw, rv);
+		}
 	}
 	return !!rv;
 }
 
-static void update_flow_timers(struct flow_infos *flow_info, struct dst_infos *dst_infos)
+static __always_inline void update_flow_timers(struct flow_infos *flow_info, struct dst_infos *dst_infos)
 {
 	// Timers
 	__u64 ecn_time = 0; // TODO Try ebpf random function
@@ -43,26 +50,24 @@ static void update_flow_timers(struct flow_infos *flow_info, struct dst_infos *d
 
 	flow_info->wait_backoff_max = flow_info->wait_backoff_max * 2; // Exponentially increase the maximum backoff time
 
-	bpf_debug("RANDOM = %lu\n", ecn_time);
+	//bpf_debug("RANDOM = %lu\n", ecn_time);
 	flow_info->wait_before_move = ecn_time;
 }
 
 SEC("sockops")
 int handle_sockop(struct bpf_sock_ops *skops)
 {
-	struct ip6_srh_t *srh;
 	struct srh_record_t *srh_record;
 	struct dst_infos *dst_infos;
 	struct flow_infos *flow_info;
 	struct flow_tuple flow_id;
-	struct params_better_path args_bp;
-	char srh_buf[72]; // room for 4 segment
-	char dest_addr_buf[255];
+	floating float_tmp;
 
 	int op;
 	int rv = 0;
 	__u32 key = 0;
 	__u64 cur_time;
+	__u32 ecount;
 
 	cur_time = bpf_ktime_get_ns();
 	op = (int) skops->op;
@@ -76,36 +81,62 @@ int handle_sockop(struct bpf_sock_ops *skops)
 	flow_info = (void *)bpf_map_lookup_elem(&conn_map, &flow_id);
 
 	if (!flow_info) {  // TODO Problem if listening connections => no destination defined !!!
-		int ret;
 		struct flow_infos new_flow;
 
 		//bpf_debug("flow not found, adding it\n");
-		new_flow.srh_id = get_best_dest_path(&dest_map, (struct ip6_addr_t *) flow_id.remote_addr);
+		new_flow.srh_id = 0; // TODO Call EXP3
 		new_flow.last_reported_bw = 0;
 		new_flow.sample_start_time = cur_time;
 		new_flow.sample_start_bytes = skops->snd_una;
 		new_flow.last_move_time = cur_time;
 		new_flow.first_loss_time = 0;
 		new_flow.number_of_loss = 0;
-
+		new_flow.ecn_count = 0;
+		new_flow.rtt_count = 0;
+		new_flow.last_ecn_rtt = 0;
+		new_flow.exp3_last_number_actions = 0;
+		new_flow.exp3_curr_reward = 0;
+		new_flow.exp3_last_probability.mantissa = 1 << 31;
+		new_flow.exp3_last_probability.exponent = 0;
+		//__u32 integer_prob = 0;
+		//__u32 decimal_prob = 0;
+		//floating_to_u32s(new_flow.exp3_last_probability, &integer_prob, &decimal_prob);
+		//bpf_debug("Init flow Last proba: %u.0*%u\n", integer_prob, decimal_prob);
+		//bpf_debug("Init flow MAX_SRH_BY_DEST %u\n", MAX_SRH_BY_DEST);
+		floating max_paths;
+		to_floating(MAX_SRH_BY_DEST - 1, 0, 1, &max_paths);
+		//__u32 integer_weight = 0;
+		//__u32 decimal_weight = 0;
+		new_flow.exp3_weigth_mantissa_0 = 1; //TODO This fails !!!! // float_tmp.mantissa;
+		new_flow.exp3_weigth_exponent_0 = 0; //float_tmp.exponent;
+		new_flow.exp3_weigth_mantissa_1 = 1; //TODO This fails !!!! // float_tmp.mantissa;
+		new_flow.exp3_weigth_exponent_1 = 0; //float_tmp.exponent;
+		new_flow.exp3_weigth_mantissa_2 = 1; //TODO This fails !!!! // float_tmp.mantissa;
+		new_flow.exp3_weigth_exponent_2 = 0; //float_tmp.exponent;
+		new_flow.exp3_weigth_mantissa_3 = 1; //TODO This fails !!!! // float_tmp.mantissa;
+		new_flow.exp3_weigth_exponent_3 = 0; //float_tmp.exponent;
+		//bpf_debug("Init flow weight %u\n", j);
+		//floating_to_u32s(new_flow.exp3_weigths[j], &integer_weight, &decimal_weight);
+		//bpf_debug("Debug mantissa: %llu\n", float_tmp.mantissa);
 		// Timers
 		new_flow.wait_backoff_max = WAIT_BEFORE_INITIAL_MOVE;
 		dst_infos = (void *) bpf_map_lookup_elem(&dest_map, flow_id.remote_addr);
 		if (!dst_infos)
-			return 1;
+			return 1; // Listening connections
 
 		move_path(dst_infos, new_flow.srh_id, skops);
 		update_flow_timers(&new_flow, dst_infos);
 
 		// Insert flow to map
-		ret = bpf_map_update_elem(&conn_map, &flow_id, &new_flow, BPF_ANY);
-		if (ret) 
+		rv = bpf_map_update_elem(&conn_map, &flow_id, &new_flow, BPF_ANY);
+		if (rv) 
 			return 1;
 		flow_info = (void *)bpf_map_lookup_elem(&conn_map, &flow_id);
 		if (!flow_info)
 			return 1;
 		//bpf_debug("Flow path is correctly entered !\n");
-		bpf_debug("Select path ID : %lu - src %u.%u\n", new_flow.srh_id, flow_id.local_addr[0], flow_id.local_addr[1]);
+		//bpf_debug("Select path ID : %lu - src %u.%u\n", new_flow.srh_id, flow_id.local_addr[0], flow_id.local_addr[1]);
+		//floating_test_all(); // TODO Remove
 	}
 
 	//bpf_debug("segs_out: %lu packets: %lu interval: %lu\n", skops->segs_out, skops->rate_delivered, skops->rate_interval_us);
@@ -125,10 +156,10 @@ int handle_sockop(struct bpf_sock_ops *skops)
 				bpf_map_delete_elem(&conn_map, &flow_id);
 			}
 			break;
-		case BPF_SOCK_OPS_RWND_INIT:
+		/*case BPF_SOCK_OPS_RWND_INIT:
 		case BPF_SOCK_OPS_TIMEOUT_INIT:
-			return 0;
-		case BPF_SOCK_OPS_TCP_XMIT:
+			return 0;*/
+		/*case BPF_SOCK_OPS_TCP_XMIT:
 			//bpf_debug("currtime: %llu start time : %llu\n", cur_time, flow_info->sample_start_time);
 			//bpf_debug("una: %lu, start: %lu, bytes envoyes : %lu\n",skops->snd_una, flow_info->sample_start_bytes, skops->snd_una - flow_info->sample_start_bytes);
 			// More than 1/10 second has passed, let's check
@@ -151,36 +182,105 @@ int handle_sockop(struct bpf_sock_ops *skops)
 					// bpf_map_update_elem(&dest_map, flow_id.remote_addr, dst_infos, BPF_ANY);
 				//}
 			//}
-			break;
-		case BPF_SOCK_OPS_UDP_XMIT:
-			break;
-		case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
-			//bpf_debug("Active established !!!\n");
-		case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+			break;*/
+		/*case BPF_SOCK_OPS_UDP_XMIT:
+			break;*/
+		/*case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+			//bpf_debug("Active established !!!\n");*/
+		/*case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
 			//bpf_debug("Una initial: %lu\n", skops->snd_una);
 			//bpf_debug("Passive or active established !!!\n");
 			flow_info->sample_start_time = cur_time;
 			flow_info->sample_start_bytes = skops->snd_una;
 			bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-			break;
-
+			break;*/
 		case BPF_SOCK_OPS_TCP_CONNECT_CB:
-			bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG); // TCP State change
-			//bpf_sock_ops_cb_flags_set(skops, (BPF_SOCK_OPS_RETRANS_CB_FLAG|BPF_SOCK_OPS_RTO_CB_FLAG));
-			dst_infos = (void *)bpf_map_lookup_elem(&dest_map, flow_id.remote_addr);
+			//bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG); // TCP State change
+			bpf_sock_ops_cb_flags_set(skops, (BPF_SOCK_OPS_RETRANS_CB_FLAG|BPF_SOCK_OPS_RTO_CB_FLAG|BPF_SOCK_OPS_RTT_CB_FLAG));
+			/*dst_infos = (void *)bpf_map_lookup_elem(&dest_map, flow_id.remote_addr);
 			if (dst_infos && flow_info->srh_id >= 0 && flow_info->srh_id < MAX_SRH_BY_DEST) {
 				srh_record = &dst_infos->srhs[flow_info->srh_id];
 				rv = bpf_setsockopt(skops, SOL_IPV6, IPV6_RTHDR,
 						&srh_record->srh, sizeof(srh_buf));
 				//bpf_debug("CONNECT - setsockopt return value %d\n", rv);
-			}
+			}*/
 			break;
-
+		case BPF_SOCK_OPS_RTT_CB:
+			// This RTT count is useful to determine the congestion level
+			flow_info->rtt_count += 1;
+			rv = bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
+			if (rv)
+				return 1;
+			break;
+		/*case BPF_SOCK_OPS_ECN_ECE:
+			//bpf_debug("ECN received\n");
+			break;*/
 		case BPF_SOCK_OPS_ECN_CE:
-			bpf_debug("Congestion experienced\n");
+			//bpf_debug("Congestion experienced %lu %lu\n", flow_info->rtt_count, flow_info->last_ecn_rtt);
+			if (flow_info->rtt_count > flow_info->last_ecn_rtt + 100) {
+				flow_info->ecn_count = 0;
+			} else {
+				//bpf_debug("Consecutive congestion experienced\n");
+			}
+			flow_info->last_ecn_rtt = flow_info->rtt_count;
+
+			ecount = flow_info->ecn_count;
+			flow_info->ecn_count = ecount + 1;
 
 			// We already moved less than X seconds ago... do nothing
-			if ((cur_time - flow_info->last_move_time) < flow_info->wait_before_move)
+			if (flow_info->ecn_count < 3 || (cur_time - flow_info->last_move_time) < flow_info->wait_before_move) {
+				// TODO Remove if ((cur_time - flow_info->last_move_time) < 3000000000)
+				rv = bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
+				if (rv)
+					return 1;
+				break;
+			}
+			flow_info->ecn_count = 0;
+
+			//bpf_debug("Congestion experienced - Try to change from %u\n", flow_info->srh_id);
+			key = exp3_next_path(&dest_map, flow_info, flow_id.remote_addr);
+
+			// This can't be helped
+			if (key == flow_info->srh_id)
+				break;
+			//bpf_debug("Congestion experienced 3 - Changing in progress\n");
+
+			// Get the infos for the current path and remove our bw
+			dst_infos = (void *)bpf_map_lookup_elem(&dest_map, flow_id.remote_addr);
+			if (dst_infos) {
+				//bpf_debug("Congestion experienced 4 - dst_infos found!\n");
+
+				// Then move to the next path
+				rv = move_path(dst_infos, key, skops);
+				if (!rv) {
+					// Update flow informations
+					flow_info->srh_id = key;
+					flow_info->last_move_time = cur_time;
+					flow_info->first_loss_time = 0;
+					flow_info->number_of_loss = 0;
+
+					update_flow_timers(flow_info, dst_infos);
+					bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
+					//bpf_debug("Congestion experienced 6 - src %u.%u - Changing to %u\n",
+					//          flow_id.local_addr[0], flow_id.local_addr[1], key);
+					//bpf_debug("Congestion experienced 7 - after updating flow info\n");
+				}
+				//bpf_debug("Congestion experienced 8 - Change finished !\n");
+			}
+			break;
+		/*case BPF_SOCK_OPS_NEEDS_ECN:
+			//bpf_debug("Need ECN called\n");
+			return 1;*/
+
+		/*case BPF_SOCK_OPS_RTO_CB:
+			bpf_debug("Restrans called\n");
+
+			ecount = flow_info->ecn_count;
+			if (ecount < 3)
+				flow_info->ecn_count = ecount + 1;
+
+			// We already moved less than X seconds ago... do nothing
+			if (flow_info->ecn_count < 3 || (cur_time - flow_info->last_move_time) < flow_info->wait_before_move)
 			// TODO Remove if ((cur_time - flow_info->last_move_time) < 3000000000)
 				break;
 
@@ -208,98 +308,13 @@ int handle_sockop(struct bpf_sock_ops *skops)
 
 					update_flow_timers(flow_info, dst_infos);
 					bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-					bpf_debug("Congestion experienced 6 - src %u.%u - Changing to %u\n",
+					bpf_debug("Congestion experienced RTO - src %u.%u - Changing to %u\n",
 					          flow_id.local_addr[0], flow_id.local_addr[1], key);
 					//bpf_debug("Congestion experienced 7 - after updating flow info\n");
 				}
 				//bpf_debug("Congestion experienced 8 - Change finished !\n");
 			}
-			break;
-		case BPF_SOCK_OPS_NEEDS_ECN:
-			//bpf_debug("Need ECN called\n");
-			return 1;
-
-		/*case BPF_SOCK_OPS_RETRANS_CB:
-			bpf_debug("Restrans called\n");
-
-			// We already moved less than 3 seconds ago... do nothing
-			if ((cur_time - flow_info->last_move_time) < 3000000000)
-				break;
-			
-			// If this is the first time we experience a loss for this sample
-			if (flow_info->first_loss_time == 0) {
-				flow_info->first_loss_time = cur_time;
-				flow_info->number_of_loss = 1;
-				bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-				break;
-			}
-
-			flow_info->number_of_loss++;	
-
-			// If we experienced more than 3 losses in 1 second
-			if ((cur_time - flow_info->first_loss_time) > 1000000000) {
-				flow_info->first_loss_time = cur_time;
-				flow_info->number_of_loss = 1;
-				bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-				break;
-			} else {
-				// It's been lesst than 1 second
-				if (flow_info->number_of_loss < 4) {
-					bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-					break;	
-				}
-			}
-
-			args_bp.self_allowed = 1;
-			args_bp.dst_addr = (struct ip6_addr_t *) &flow_id.remote_addr;
-			key = get_better_dest_path(&dest_map, flow_info, &args_bp);
-
-			// If we already are on the best path
-			if (key == flow_info->srh_id) {
-				// If this pith isn't THAT bad, let's stay
-				if (flow_info->number_of_loss < 10)
-					break;
-
-				// Try a different path
-				args_bp.self_allowed = 0;
-				args_bp.dst_addr = (struct ip6_addr_t *) flow_id.remote_addr;
-				key = get_better_dest_path(&dest_map, flow_info, &args_bp);
-
-				// Couldn't get a best path, too bad, nothing we can do
-				if (key == flow_info->srh_id)
-					break;
-			}
-
-			// First, remove our info from the previous path
-			dst_infos = (void *)bpf_map_lookup_elem(&dest_map, flow_id.remote_addr);
-			if (dst_infos) {
-				if (flow_info->srh_id >= 0 && flow_info->srh_id < MAX_SRH_BY_DEST) {
-					srh_record = &dst_infos->srhs[flow_info->srh_id];
-					//srh_record->curr_bw = srh_record->curr_bw - flow_info->last_reported_bw; // TODO Problem if no more bandwidth and we took that out of despair
-					bpf_map_update_elem(&dest_map, flow_id.remote_addr, dst_infos, BPF_ANY);
-				}
-
-				// Then move to the next path
-				if (key >= 0 && key < MAX_SRH_BY_DEST) {
-					srh_record = &dst_infos->srhs[key];
-					rv = bpf_setsockopt(skops, SOL_IPV6, IPV6_RTHDR,
-							&srh_record->srh, sizeof(srh_buf));
-					if (!rv) {
-						// Update flow informations
-						flow_info->srh_id = key;
-						flow_info->last_move_time = cur_time;
-						flow_info->first_loss_time = 0;
-						flow_info->number_of_loss = 0;
-						bpf_map_update_elem(&conn_map, &flow_id, flow_info, BPF_ANY);
-
-						// Update the new path bw
-						//srh_record->curr_bw = srh_record->curr_bw + flow_info->last_reported_bw; // TODO Problem if no more bandwidth and we took that out of despair
-						bpf_map_update_elem(&dest_map, flow_id.remote_addr, dst_infos, BPF_ANY);
-					}
-				}
-			}
 			break;*/
-
 	}
 	skops->reply = rv;
 
--- utils.h
+++ utils.h
@@ -1,11 +1,19 @@
+#ifndef EBPF_UTILS_H
+#define EBPF_UTILS_H
+
+#include "bpf_helpers.h"
+#include "floating_point.h"
+
 /* Defining constant values */
 
 #define IPPROTO_TCP 	6 /* TCP protocol in HDR */
 #define AF_INET6 		10 /* IPv6 HDR */
 #define SOL_IPV6 		41 /* IPv6 Sockopt */
+#define SOL_SOCKET		1 /* Socket Sockopt */
+#define SO_MAX_PACING_RATE	47 /* Max pacing rate for setsockopt */
 #define IPV6_RTHDR 		57 /* SRv6 Option for sockopt */
 #define ETH_HLEN 		14 /* Ethernet hdr length */
-#define DEBUG 			1
+// #define DEBUG 			1
 #define PIN_NONE		0
 #define PIN_GLOBAL_NS	2
 #define MAX_SRH			50
@@ -15,6 +23,13 @@
 
 #define WAIT_BEFORE_INITIAL_MOVE 1000000000 // 1 sec
 #define WAIT_BACKOFF 2 // Multiply by two the waiting time whenever a path change is made
+
+// Float constant multiplier to make float operations work with integer
+#define FLOAT_MULT 1000000000
+
+// Exp3 GAMMA
+#define GAMMA(x) to_floating(0, 5, 1, &x) // 0.5
+#define GAMMA_REV(x) to_floating(2, 0, 1, &x) // 2
 
 /* eBPF definitions */
 #ifndef __section
@@ -29,6 +44,7 @@
 
 #define SEC(NAME) __attribute__((section(NAME), used))
 
+#define DEBUG
 #ifdef  DEBUG
 /* Only use this for debug output. Notice output from bpf_trace_printk()
  *  * end-up in /sys/kernel/debug/tracing/trace_pipe
@@ -40,7 +56,7 @@
 			##__VA_ARGS__);					\
 			})
 #else
-#define bpf_debug(fmt, ...) { } while (0)
+#define bpf_debug(fmt, ...) { } while (0);
 #endif
 
 #define htonll(x) ((bpf_htonl(1)) == 1 ? (x) : ((uint64_t)bpf_htonl((x) & \
@@ -93,7 +109,61 @@ struct flow_infos {
 	__u64 wait_before_move; // current waiting time
 	__u64 first_loss_time;
 	__u32 number_of_loss;
+	__u64 rtt_count; // Count the number of RTT in the connection, this is useful to know if congestion signals are consecutive or not
+	__u32 ecn_count; // Count the number of consecutive CWR sent (either from ECN or other causes)
+	__u64 last_ecn_rtt; // The index of the last RTT were we sent an CWR
+	__u32 exp3_last_number_actions;
+	__u32 exp3_curr_reward;
+	floating exp3_last_probability;
+	__u64 exp3_weigth_mantissa_0; // Current weight for each path
+	__u32 exp3_weigth_exponent_0;
+	__u64 exp3_weigth_mantissa_1; // Current weight for each path
+	__u32 exp3_weigth_exponent_1;
+	__u64 exp3_weigth_mantissa_2; // Current weight for each path
+	__u32 exp3_weigth_exponent_2;
+	__u64 exp3_weigth_mantissa_3; // Current weight for each path
+	__u32 exp3_weigth_exponent_3;
 } __attribute__((packed));
+
+#define exp3_weigth_mantissa_set(flow_infos, idx, value) \
+	if (idx == 0) \
+		flow_infos->exp3_weigth_mantissa_0 = value; \
+	else if (idx == 1) \
+		flow_infos->exp3_weigth_mantissa_1 = value; \
+	else if (idx == 2) \
+		flow_infos->exp3_weigth_mantissa_2 = value; \
+	else \
+		flow_infos->exp3_weigth_mantissa_3 = value;
+
+#define exp3_weigth_exponent_set(flow_infos, idx, value) \
+	if (idx == 0) \
+		flow_infos->exp3_weigth_exponent_0 = value; \
+	else if (idx == 1) \
+		flow_infos->exp3_weigth_exponent_1 = value; \
+	else if (idx == 2) \
+		flow_infos->exp3_weigth_exponent_2 = value; \
+	else \
+		flow_infos->exp3_weigth_exponent_3 = value;
+
+#define exp3_weigth_mantissa_get(flow_infos, idx, value) \
+	if (idx == 0) \
+		value = flow_infos->exp3_weigth_mantissa_0; \
+	else if (idx == 1) \
+		value = flow_infos->exp3_weigth_mantissa_1; \
+	else if (idx == 2) \
+		value = flow_infos->exp3_weigth_mantissa_2; \
+	else \
+		value = flow_infos->exp3_weigth_mantissa_3;
+
+#define exp3_weigth_exponent_get(flow_infos, idx, value) \
+	if (idx == 0) \
+		value = flow_infos->exp3_weigth_exponent_0; \
+	else if (idx == 1) \
+		value = flow_infos->exp3_weigth_exponent_1; \
+	else if (idx == 2) \
+		value = flow_infos->exp3_weigth_exponent_2; \
+	else \
+		value = flow_infos->exp3_weigth_exponent_3;
 
 struct dst_infos {
 	struct ip6_addr_t dest;
@@ -110,12 +180,7 @@ struct bpf_elf_map {
 	__u32 pinning;
 } __attribute__((packed));
 
-struct params_better_path {
-	int self_allowed;
-	struct ip6_addr_t *dst_addr;
-} __attribute__((packed));
-
-static __always_inline void get_flow_id_from_sock(struct flow_tuple *flow_id, struct bpf_sock_ops *skops) {
+static void get_flow_id_from_sock(struct flow_tuple *flow_id, struct bpf_sock_ops *skops) {
 	flow_id->family = skops->family;
 	flow_id->local_addr[0] = skops->local_ip6[0];
 	flow_id->local_addr[1] = skops->local_ip6[1];
@@ -129,7 +194,7 @@ static __always_inline void get_flow_id_from_sock(struct flow_tuple *flow_id, st
 	flow_id->remote_port = bpf_ntohl(skops->remote_port);
 }
 
-static __always_inline uint32_t get_best_dest_path(struct bpf_elf_map *dt_map, struct ip6_addr_t *dst_addr) {
+/*static __always_inline uint32_t get_best_dest_path(struct bpf_elf_map *dt_map, struct ip6_addr_t *dst_addr) {
 	uint64_t lowest_delay = 0;
 	uint32_t lowest_id = 0, current_delay = 0;
 	struct srh_record_t *srh_record = NULL;
@@ -139,21 +204,21 @@ static __always_inline uint32_t get_best_dest_path(struct bpf_elf_map *dt_map, s
 
 	dst_infos = (void *) bpf_map_lookup_elem(dt_map, dst_addr);
 	if (!dst_infos) {
-		bpf_debug("Cannot find the destination entry => Cannot find another SRH\n");
+		//bpf_debug("Cannot find the destination entry => Cannot find another SRH\n");
 		return lowest_id;
 	}
 
 	if (lowest_id >=0 && lowest_id < MAX_SRH_BY_DEST) {
 		srh_record = &dst_infos->srhs[lowest_id];
 		if (!srh_record) {
-			bpf_debug("Cannot find the SRH entry\n");
+			//bpf_debug("Cannot find the SRH entry\n");
 		} else {
 			lowest_delay = srh_record->delay;
 		}
 	}
 
-	#pragma clang loop unroll(full)
-	for (unsigned int i = firsti; i < MAX_SRH_BY_DEST; i++) {
+	//#pragma clang loop unroll(full)
+	for (unsigned int i = firsti; i <= MAX_SRH_BY_DEST - 1; i++) {
 		int j = i; // Compiler cannot unroll otherwise
 		srh_record = &dst_infos->srhs[i];
 
@@ -177,9 +242,9 @@ static __always_inline uint32_t get_best_dest_path(struct bpf_elf_map *dt_map, s
 
 	}
 	return lowest_id;
-}
+}*/
 
-static __always_inline uint32_t get_better_dest_path(struct bpf_elf_map *dt_map, struct flow_infos *flow_info, int self_allowed, __u32 *dst_addr) {
+/*static uint32_t get_better_dest_path(struct bpf_elf_map *dt_map, struct flow_infos *flow_info, int self_allowed, __u32 *dst_addr) {
 	uint64_t lowest_delay = 0;
 	uint32_t lowest_id = flow_info->srh_id, current_delay = 0;
 	struct srh_record_t *srh_record = NULL;
@@ -188,14 +253,14 @@ static __always_inline uint32_t get_better_dest_path(struct bpf_elf_map *dt_map,
 
 	dst_infos = (void *) bpf_map_lookup_elem(dt_map, dst_addr);
 	if (!dst_infos) {
-		bpf_debug("Cannot find the destination entry => Cannot find another SRH\n");
+		//bpf_debug("Cannot find the destination entry => Cannot find another SRH\n");
 		return lowest_id;
 	}
 
 	if (lowest_id >=0 && lowest_id < MAX_SRH_BY_DEST) {
 		srh_record = &dst_infos->srhs[lowest_id];
 		if (!srh_record) {
-			bpf_debug("Cannot find the SRH entry\n");
+			//bpf_debug("Cannot find the SRH entry\n");
 		} else {
 			lowest_delay = srh_record->delay;
 		}
@@ -229,6 +294,172 @@ static __always_inline uint32_t get_better_dest_path(struct bpf_elf_map *dt_map,
 
 	}
 	return lowest_id;
+}*/
+
+static __always_inline void exp3_reward_path(struct flow_infos *flow_info) {
+	/*
+	theReward = reward(choice, t)
+	weights[choice] *= math.exp(theReward / (probabilityDistribution[choice] * gamma_rev * numActions)) # important that we use estimated reward here!
+	*/
+	floating gamma_rev;
+	floating reward;
+	floating exponent_den_factor;
+	floating exponent_den;
+	floating nbr_actions;
+	floating exponent;
+	floating weight_factor;
+	floating float_tmp, float_tmp2;
+
+	GAMMA_REV(gamma_rev);
+
+	to_floating(flow_info->exp3_curr_reward, 0, 1, &reward); // TODO Compute reward
+	to_floating(flow_info->exp3_last_number_actions, 1, 0, &nbr_actions);
+	floating_multiply(flow_info->exp3_last_probability, gamma_rev, &exponent_den_factor);
+	floating_multiply(exponent_den_factor, nbr_actions, &exponent_den);
+	floating_divide(reward, exponent_den, &exponent);
+	float_e_power_a(exponent, &weight_factor);
+	// TODO Remove
+	flow_info->exp3_weigth_mantissa_1 = weight_factor.mantissa;
+	flow_info->exp3_weigth_exponent_1 = weight_factor.exponent;
+	//weight_factor.mantissa = exponent.mantissa;
+	//weight_factor.exponent = exponent.exponent;
+	// TODO Remove
+
+	__u32 idx = flow_info->srh_id;
+	if (idx >= 0 && idx <= MAX_SRH_BY_DEST - 1) { // Always true but this is for eBPF loader
+		exp3_weigth_mantissa_get(flow_info, idx, float_tmp.mantissa);
+		exp3_weigth_exponent_get(flow_info, idx, float_tmp.exponent);
+		floating_multiply(float_tmp, weight_factor, &float_tmp2);
+		exp3_weigth_mantissa_set(flow_info, idx, float_tmp2.mantissa);
+		exp3_weigth_exponent_set(flow_info, idx, float_tmp2.exponent);
+	}
+}
+
+static __always_inline __u32 exp3_next_path(struct bpf_elf_map *dt_map, struct flow_infos *flow_info, __u32 *dst_addr) {
+	/*
+	def distr(weights, gamma=0.0):
+		theSum = float(sum(weights))
+		return tuple((1.0 - gamma) * (w / theSum) + (gamma / len(weights)) for w in weights)
+
+	def exp3(numActions, reward, gamma):
+		weights = [1.0] * numActions
+
+		t = 0
+		while True:
+			probabilityDistribution = distr(weights, gamma)
+			choice = draw(probabilityDistribution)
+			theReward = reward(choice, t)
+
+			estimatedReward = theReward / probabilityDistribution[choice]
+			weights[choice] *= math.exp(estimatedReward * gamma / numActions) # important that we use estimated reward here!
+
+			yield choice, theReward, estimatedReward, weights
+			t = t + 1
+	*/
+	floating gamma;
+	GAMMA(gamma);
+
+	__u32 chosen_id = 1, current_delay = 0;
+	struct srh_record_t *srh_record = NULL;
+	struct dst_infos *dst_infos = NULL;
+
+	dst_infos = (void *) bpf_map_lookup_elem(dt_map, dst_addr);
+	if (!dst_infos) {
+		//bpf_debug("Cannot find the destination entry => Cannot find another SRH\n");
+		return chosen_id;
+	}
+
+	// Compute the reward of the previous path
+	exp3_reward_path(flow_info);
+
+	// Compute the sum of weights
+	/*floating sum;
+	to_floating(0, 0, 1, &sum);
+	__u32 nbr_valid_paths = 0;
+	#pragma clang loop unroll(full)
+	for (__u32 i = 0; i <= MAX_SRH_BY_DEST - 1; i++) {
+		int xxx = i; // Compiler cannot unroll otherwise
+		srh_record = &dst_infos->srhs[i];
+
+		// Wrong SRH ID -> might be inconsistent state, so skip
+		// Not a valid SRH for the destination
+		// Same SRH
+		if (!srh_record || !srh_record->srh.type) {  // 1
+			//bpf_debug("Cannot find the SRH entry indexed at %d at a dest entry\n", i);
+			continue;
+		}
+
+		if (!srh_record->is_valid) {  // 1
+			//bpf_debug("SRH entry indexed at %d by the dest entry is invalid\n", i);
+			continue; // Not a valid SRH for the destination
+		}
+
+		if (!flow_info || srh_record->srh_id == flow_info->srh_id) {  // 1
+			continue;
+		}
+
+		floating_add(sum, flow_info->exp3_weigths[xxx], &sum);
+		nbr_valid_paths += 1;
+	}*/
+
+	// TODO Compute the probabilities
+	/*floating probability;
+	floating one;
+	to_floating(1, 0, 1, &one);
+	floating one_minus_gamma;
+	floating_add(one, gamma, &one_minus_gamma);
+	floating weight_times_gama;
+	floating term1;
+	floating valid_paths;
+	to_floating(nbr_valid_paths, 0, 1, &valid_paths);
+	floating term2;
+	floating_divide(gamma, valid_paths, &term2);
+
+	__u64 pick = ((__u64) bpf_get_prandom_u32()) % FLOAT_MULT; // No problem if FLOAT_MULT < UIN32T_MAX
+	__u64 accumulator = 0;
+	__u32 integer_part;
+	__u32 decimal_part;
+
+	#pragma clang loop unroll(full)
+	for (__u32 i = 0; i <= MAX_SRH_BY_DEST - 1; i++) {
+		int yyy = i; // Compiler cannot unroll otherwise
+		srh_record = &dst_infos->srhs[i];
+
+		// Wrong SRH ID -> might be inconsistent state, so skip
+		// Not a valid SRH for the destination
+		// Same SRH
+		if (!srh_record || !srh_record->srh.type) {  // 2
+			//bpf_debug("Cannot find the SRH entry indexed at %d at a dest entry\n", i);
+			continue;
+		}
+
+		if (!srh_record->is_valid) {  // 2
+			//bpf_debug("SRH entry indexed at %d by the dest entry is invalid\n", i);
+			continue; // Not a valid SRH for the destination
+		}
+
+		if (!flow_info || srh_record->srh_id == flow_info->srh_id) {  // 2
+			continue;
+		}
+
+		// prob[i] = (1.0 - gamma) * (w[i] / theSum) + (gamma / len(weights))
+		floating_multiply(one_minus_gamma, flow_info->exp3_weigths[yyy], &term1); // TODO , &weight_times_gama);
+		//floating_divide(weight_times_gama, sum, &term1); // TODO Instructions that makes everything overflow
+		floating_add(term1, term2, &probability);
+*/
+		/*floating_to_u32s(probability, &integer_part, &decimal_part);
+		accumulator += decimal_part; // No need to take the integer part since these are numbers in [0, 1[
+		if (pick < accumulator) {
+			// We found the chosen one
+			chosen_id = i;
+			flow_info->exp3_last_probability.mantissa = probability.mantissa;
+			flow_info->exp3_last_probability.exponent = probability.exponent;
+			break;
+		}*/
+	/*}
+
+	flow_info->exp3_last_number_actions = nbr_valid_paths;*/
+	return chosen_id;
 }
 
 struct bpf_elf_map SEC("maps") conn_map = {
@@ -246,3 +477,5 @@ struct bpf_elf_map SEC("maps") dest_map = {
 	.pinning	= PIN_NONE,
 	.max_elem	= MAX_FLOWS,
 };
+
+#endif
